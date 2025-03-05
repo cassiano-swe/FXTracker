deeb704227768d8a9ca7a9f09855e8415d04d662
Fernando Cassiano <cassiano.fernando@outlook.com>
2025-03-05 09:58:39 -0300

    chore: fix domain attributes names

--- src/FXTracker.domain/Entities/Currency.cs
+++ src/FXTracker.domain/Entities/Currency.cs
@@ -1,2 +1,2 @@
 namespace FXTracker.domain.Entities;
-public record Currency (Guid Id, double Timestamp, string Symbol, DateTime Date, Dictionary<string, double> Parity);
+public record Currency (Guid Id, double Timestamp, string BaseCurrency, DateTime Date, string TargetCurrency, double Quote);
